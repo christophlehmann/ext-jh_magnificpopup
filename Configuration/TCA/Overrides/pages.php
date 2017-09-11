50e59cde78b05a80a76d7025da8bc1a0790e5d68
Christoph Lehmann <lehmann@networkteam.com>
2017-09-11 15:10:30 +0200

    TASK: Do not load RTE configuration by default
    They are now loaded via page tsconfig preset and do
    not break ckeditor styles

---
+++ Configuration/TCA/Overrides/pages.php
@@ -0,0 +1,13 @@
+<?php
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+	'jh_magnificpopup',
+	'Configuration/TsConfig/Page/Links/PageTsConfig.txt',
+	'Magnific Popup : Load RTE link classes (rtehtmlarea)'
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+	'jh_magnificpopup',
+	'Configuration/TsConfig/Page/AlphaFeatures/PageTsConfig.txt',
+	'Magnific Popup : Alpha Features :: May change in the future, not for production usage'
+);
