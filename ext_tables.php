dc21928f0e4b526a69bce36238b57986c0479503
Christoph Lehmann <lehmann@networkteam.com>
2020-06-29 16:00:21 +0200

    [TASK] Replace

--- ext_tables.php
+++ ext_tables.php
@@ -8,23 +8,23 @@ $confArr = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['jh_magnifi
 
 // Add frontend plugin
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'JonathanHeilmann.' . $_EXTKEY,
+    'JonathanHeilmann.jh_magnificpopup',
     'Pi1',
     'Magnific Popup'
 );
 
 // Add flexform for frontend plugin
-$extensionName = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY);
+$extensionName = \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase('jh_magnificpopup');
 $pluginSignature = strtolower($extensionName) . '_pi1';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature,
-    'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/MagnificpopupPlugin.xml');
+    'FILE:EXT:jh_magnificpopup/Configuration/FlexForms/MagnificpopupPlugin.xml');
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,recursive';
 
 // Add static files
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/Default',
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('jh_magnificpopup', 'Configuration/TypoScript/Default',
     'Magnific Popup');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/Inline',
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('jh_magnificpopup', 'Configuration/TypoScript/Inline',
     'Magnific Popup - Content Element');
 
 // Add colPos for content elements
@@ -32,7 +32,7 @@ if (TYPO3_MODE == 'BE') {
     if (!isset($GLOBALS['TCA']['tt_content']['columns']['colPos']['config']['items'][$confArr['colPosOfIrreContent']])) {
         // Add the new colPos to the array, only if the ID does not exist...
         $GLOBALS['TCA']['tt_content']['columns']['colPos']['config']['items'][$confArr['colPosOfIrreContent']] = array(
-            $_EXTKEY,
+            'jh_magnificpopup',
             $confArr['colPosOfIrreContent']
         );
     }
